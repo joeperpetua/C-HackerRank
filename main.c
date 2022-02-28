89e94627606a83325662563d5346956077f63a2f
joeperpetua <qwerty.perpetua@gmail.com>
2022-02-28 14:44:03 +0100

    Added e3 and e4

--- main.c
+++ main.c
@@ -40,11 +40,47 @@ void e2()
 }
 
 void e3() {
+    // Var declaration
+    int numA, numB;
+    float numC, numD;
 
+    // scan values
+    printf("\nInsert 2 int values\n");
+    scanf_s("%d %d", &numA, &numB);
+
+    printf("\nInsert 2 float values\n");
+    scanf_s("%f %f", &numC, &numD);
+
+
+    // print sum and diff of int
+    printf("\nint values:\nSum: %d\nDifference: %d\n", numA + numB, numA - numB);
+
+    // print sum and diff diff of float
+    printf("\nfloat values:\nSum: %.1f\nDifference: %.1f\n", (double)numC + (double)numD, (double)numC - (double)numD);
     return;
 }
 
+int max_of_four(int a, int b, int c, int d) {
+    int numbers[4] = {a, b, c, d }, max = 0;
+    int numbers_length = (sizeof(numbers) / sizeof(numbers[0]));
+
+    for (int i = 0; i < numbers_length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+
+    return max;
+}
+
 void e4() {
+    int a, b, c, d;
+
+    printf("\nInsert 4 int values\n");
+    scanf_s("%d %d %d %d", &a, &b, &c, &d);
+
+    int ans = max_of_four(a, b, c, d);
+    printf("The highest value is %d", ans);
 
     return;
 }
@@ -54,7 +90,6 @@ int main()
     int selection;
 
     printf("Select an exercise to run. (1 to 4)\n");
-    scanf_s("\n");
     scanf_s("%d", &selection);
 
     switch (selection)
